d1fdeef48c1375fb75277276ca853cf82ef78518
kvilma <vilma.kriviciute@gmail.com>
2019-12-12 20:15:00 +0200

    pirma funkcija

--- 5lesson/Program.cs
+++ 5lesson/Program.cs
@@ -4,9 +4,26 @@ namespace _5lesson
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GetMaxNumber(1, 2, 3));
+            Console.WriteLine(GetMaxNumber(2, 2, 3));
+            Console.WriteLine(GetMaxNumber(1, 5, 6));
         }
+
+        static int GetMaxNumber(int nr1, int nr2, int nr3)
+        {
+            if ( nr1 > nr2 && nr1 > nr3 )
+            {
+                return nr1;
+            }
+            else if (nr2 > nr3)
+            {
+                return nr2;
+            }
+            return nr3;
+
+        }
+
     }
 }
