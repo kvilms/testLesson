4f173616ece46a97df49a5290d976d2abe3cc336
kvilma <vilma.kriviciute@gmail.com>
2019-12-12 19:24:52 +0200

    initial project setuo

---
+++ 5lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _5lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
